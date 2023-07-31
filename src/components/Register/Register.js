5417bf1ba35f17437b1d38868dc5c481ae4963c7
DineshChintu0106 <dineshchintu0106@gmail.com>
2023-07-31 14:47:50 +0530

    added phone no fileld in register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import './Register.css'
-import { Input } from 'antd'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import TextField from '@mui/material/TextField';
 
 export default function Register() {
 
@@ -10,8 +10,10 @@ export default function Register() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [address, setAddress] = useState('')
+  const [number, setNumber] = useState('')
   const [loading, setLoading] = useState(false)
   const [success, setSuccess] = useState('')
+
 
   const handleClick = (e) => {
     e.preventDefault()
@@ -20,7 +22,8 @@ export default function Register() {
       username,
       email,
       password,
-      address
+      address,
+      number
     }
     axios.post("http://localhost:4000/api/data", data).then((res) => {
       console.log(res)
@@ -31,18 +34,47 @@ export default function Register() {
       setEmail('')
       setPassword('')
       setAddress('')
+      setNumber("")
     }).catch(err => console.log(err))
   }
   return (
     <div className='register-container'>
       <form className='form-register col-lg-4 col-md-8 col-sm-10 col-xs-10' onSubmit={handleClick}>
         <h1>Sign UP</h1>
-        <Input type='text' placeholder="Username" className='input-field' onChange={(e) => { setUsername(e.target.value) }} value={username} />
-        <Input type='email' placeholder="Email" className='input-field' onChange={(e) => { setEmail(e.target.value) }} value={email} />
-        <Input type='password' placeholder="Password" className='input-field' onChange={(e) => { setPassword(e.target.value) }} value={password} />
-        <Input type='text' placeholder="Adderss" className='input-field' onChange={(e) => { setAddress(e.target.value) }} value={address} />
+        <TextField id="username" type='text' label="Username" variant="standard" InputProps={{
+          style: {
+            color: 'white',
+            borderBottom: '2px solid white'
+          }
+        }} InputLabelProps={{ style: { color: 'white' } }} className='input-field' onChange={(e) => { setUsername(e.target.value) }} value={username} />
+        <TextField id="email" type='email' label="Email" variant="standard" InputProps={{
+          style: {
+            color: 'white',
+            borderBottom: '2px solid white'
+          }
+        }} InputLabelProps={{ style: { color: 'white' } }} className='input-field' onChange={(e) => { setEmail(e.target.value) }} value={email}
+        />
+        <TextField id="password" type='password' label="Password" variant="standard" InputProps={{
+          style: {
+            color: 'white',
+            borderBottom: '2px solid white'
+          }
+        }} InputLabelProps={{ style: { color: 'white' } }} className='input-field' onChange={(e) => { setPassword(e.target.value) }} value={password} />
+        <TextField id="mobile" type='number' label="Mobile" variant="standard" InputProps={{
+          style: {
+            color: 'white',
+            borderBottom: '2px solid white',
+          }
+        }} InputLabelProps={{ style: { color: 'white' } }} className='input-field' onChange={(e) => { setNumber(e.target.value) }} value={number} />
+        <TextField id="adderss" type='text' label="Address" variant="standard" InputProps={{
+          style: {
+            color: 'white',
+            borderBottom: '2px solid white',
+          }
+        }} InputLabelProps={{ style: { color: 'white' } }} className='input-field' onChange={(e) => { setAddress(e.target.value) }} value={address} />
+
         {success.length > 0 && <strong className='text-success'>{success}<Link to={'/login'}>login</Link> to continre</strong>}
-        <button type='submit' className='btn btn-light' style={{ backgroundColor: 'white' }}>{loading ? "loading..." : "Submit"}</button>
+        <button type='submit' className='btn btn-light mt-3' style={{ backgroundColor: 'white' }}>{loading ? "loading..." : "Submit"}</button>
       </form>
     </div>
   )
